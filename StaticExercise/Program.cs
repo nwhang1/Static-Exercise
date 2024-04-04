7262b5c0667c2ac80d9bfcac991a605c91373dad
nwhang1 <nicholasjwhang@gmail.com>
2024-04-04 12:05:07 -0700

    static classes created, methods called in main

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double fahrenheit = 100.1;
+            double celsiusConverted = TempConverter.FahrenheitToCelsius(fahrenheit);
+            Console.WriteLine($"{fahrenheit} Fahrenheit is equal to {celsiusConverted} Celsius");
+
+            double celsius = 100.09;
+            double fahrenheitConverted = TempConverter.CelsiusToFahrenheit(celsius);
+            Console.WriteLine($"{celsius} Celsius is equal to {fahrenheitConverted} Fahrenheit");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            //googled formulas
+            return (fahrenheit - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+    }
+}
